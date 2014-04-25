c5f1403c687cdb914e29504cceee89b6ffea4369
Johnn Gaio <jj.the.awesome@gmail.com>
2014-04-24 17:23:26 -0700

    Started some API stuff

--- src/com/bronexus/model/PlayerInfo.java
+++ src/com/bronexus/model/PlayerInfo.java
@@ -1,5 +1,32 @@
 package com.bronexus.model;
 
+// dev key 4c82d2b0-91e8-4376-bfdb-de71b1bad16d
+// example url for summoner with name RiotSchmick:
+// https://prod.api.pvp.net/api/lol/na/v1.3/summoner/by-name/RiotSchmick?api_key=4c82d2b0-91e8-4376-bfdb-de71b1bad16d
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.os.StrictMode;
+
+@TargetApi(Build.VERSION_CODES.GINGERBREAD)
+@SuppressLint("NewApi")
 public class PlayerInfo
 {
 	private String summonerName;
@@ -41,9 +68,51 @@ public class PlayerInfo
         throw new CloneNotSupportedException(); 
     }
     // check if the summoner name is valid
+	//@TargetApi(Build.VERSION_CODES.GINGERBREAD)
 	public int lookupSummoner()
 	{
-		// this is where we look up the summoner name
+		// allows us to access the internet
+		//StrictMode.ThreadPolicy policy = new StrictMode.
+		//ThreadPolicy.Builder().permitAll().build();
+		//StrictMode.setThreadPolicy(policy); 
+		// look up the summoner name we are given
+		// url with the summoner name and our key
+		String url = "https:prod.api.pvp.net/api/lol/na/v1.3/summoner/by-name/RiotSchmick?api_key=4c82d2b0-91e8-4376-bfdb-de71b1bad16d";
+		//String url = "https://prod.api.pvp.net/api/lol/na/v1.3/summoner/by-name/" + summonerName + "?api_key=4c82d2b0-91e8-4376-bfdb-de71b1bad16d";
+		StringBuilder builder = new StringBuilder();
+		HttpClient client = new DefaultHttpClient();
+		HttpGet httpGet = new HttpGet(url);
+		try 
+		{
+			HttpResponse response = client.execute(httpGet);
+			StatusLine statusLine = response.getStatusLine();
+			int statusCode = statusLine.getStatusCode();
+			if (statusCode == 200)
+			{
+				HttpEntity entity = response.getEntity();
+				InputStream content = entity.getContent();
+				BufferedReader reader = new BufferedReader(new InputStreamReader(content));
+				String line;
+				while ((line = reader.readLine()) != null)
+				{
+					builder.append(line);
+				}
+			}
+			else
+			{
+				Log.e(PlayerInfo.class.toString(), "Failed to download file");
+			}
+		}
+		catch (ClientProtocolException e)
+		{
+			e.printStackTrace();
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+		// builder.toString() returns our response from the server
+		Log.e(PlayerInfo.class.toString(), builder.toString());
 		
 		// if it's successful..
 		// update the numMasteries
